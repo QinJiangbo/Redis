6838485e36288f050db8185442bed52ff80cbd11
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-06-07 23:42:40 +0800

    add Redis String class

--- src/main/java/com/qinjiangbo/StringOfRedis.java
+++ src/main/java/com/qinjiangbo/StringOfRedis.java
@@ -1,5 +1,6 @@
 package com.qinjiangbo;
 
+import java.util.List;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -11,8 +12,28 @@ public class StringOfRedis {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("localhost");
 
-        // set "name" value
+        // set "name"
         jedis.set("name", "qinjiangbo");
         System.out.println(jedis.get("name"));
+
+        // append "name"
+        jedis.append("name", " is my love");
+        System.out.println(jedis.get("name"));
+
+        // del "name"
+        jedis.del("name");
+        System.out.println(jedis.get("name"));
+
+        // multi-set "names"
+        jedis.mset("name", "qinjiangbo", "age", "15", "sex", "male");
+        System.out.println(jedis.get("name"));
+        System.out.println(jedis.get("age"));
+        System.out.println(jedis.get("sex"));
+
+        System.out.println("================");
+        List<String> values = jedis.mget("name", "age", "sex");
+        for (String value: values) {
+            System.out.println(value);
+        }
     }
 }
