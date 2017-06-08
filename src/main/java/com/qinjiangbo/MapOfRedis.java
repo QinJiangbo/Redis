6cc1785e931112370961ea100024a44ec774e30d
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-06-08 23:50:48 +0800

    add Redis HashMap class

---
+++ src/main/java/com/qinjiangbo/MapOfRedis.java
@@ -0,0 +1,26 @@
+package com.qinjiangbo;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @date: 08/06/2017 11:45 PM
+ * @author: qinjiangbo@github.io
+ */
+public class MapOfRedis {
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "qinjiangbo");
+        map.put("age", "23");
+        map.put("DingTalk", "qinjiangbo");
+        jedis.hmset("user", map);
+
+        // hash map get
+        System.out.println(jedis.hmget("user", "name"));
+    }
+}
