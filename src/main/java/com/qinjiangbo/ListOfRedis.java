2f838a02ee576072608e9348c56c193652a7804f
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-06-16 23:44:09 +0800

    add Redis List class

--- src/main/java/com/qinjiangbo/ListOfRedis.java
+++ src/main/java/com/qinjiangbo/ListOfRedis.java
@@ -1,8 +1,16 @@
 package com.qinjiangbo;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * @date: 14/06/2017 10:44 PM
  * @author: qinjiangbo@github.io
  */
 public class ListOfRedis {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost");
+
+        jedis.del("java");
+        System.out.println(jedis.lrange("java", 0, -1));
+    }
 }
