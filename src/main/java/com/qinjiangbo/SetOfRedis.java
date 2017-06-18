6b41cc9fe24ab1a028ead552c0d44056be850c43
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-06-19 00:04:36 +0800

    add Redis Set class

--- src/main/java/com/qinjiangbo/SetOfRedis.java
+++ src/main/java/com/qinjiangbo/SetOfRedis.java
@@ -14,11 +14,15 @@ public class SetOfRedis {
         jedis.sadd("redisUser", "Jim");
         jedis.sadd("redisUser", "Coco");
         jedis.sadd("redisUser", "Qi");
+        jedis.sadd("redisUser", "Nicolas");
+        jedis.sadd("redisUser", "Cruise");
         System.out.println(jedis.smembers("redisUser"));
 
         // remove
         jedis.srem("redisUser", "Qi");
         System.out.println(jedis.smembers("redisUser"));
+        System.out.println(jedis.sismember("redisUser", "Qi"));
+        System.out.println(jedis.srandmember("redisUser"));
         System.out.println(jedis.scard("redisUser"));
     }
 }
